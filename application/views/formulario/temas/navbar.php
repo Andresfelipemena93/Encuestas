36c2c58ffaceb1113689d63c5fc2591538571721
Andresfelipemena93 <andresmena9325@icloud.com>
2023-05-15 17:49:14 -0500

    se cambia cdn logo

--- application/views/formulario/temas/navbar.php
+++ application/views/formulario/temas/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light  " style="background-color: #bf000c;">
 	<a class="navbar-brand nabvar-main" href="<?php echo site_url(); ?>">
-		<img src="https://cdn.pixabay.com/photo/2023/05/07/03/27/03-27-27-851_960_720.png" class="logo_main" alt="logo ES">  
+		<img src="https://cdn.pixabay.com/photo/2023/05/07/03/27/medel-7975427_1280.png" class="logo_main" alt="logo ES">  
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
