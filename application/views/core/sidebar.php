d250165b8062f84ddaeadfac85aa24418bae53ce
Andresfelipemena93 <andresmena9325@icloud.com>
2023-05-15 17:36:49 -0500

    se cambia color del slider

--- application/views/core/sidebar.php
+++ application/views/core/sidebar.php
@@ -1,4 +1,4 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-color-navbar_slider sidebar sidebar-dark accordion" id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo site_url(); ?>">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
